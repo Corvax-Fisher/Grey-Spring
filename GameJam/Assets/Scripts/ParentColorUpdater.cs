6f0ad3fa371035dc1cbf8df3c67ab6b66a4333f1
Yannick <yannicklamprecht@live.de>
2016-11-19 22:41:48 +0100

    Colored Jumppads

---
+++ GameJam/Assets/Scripts/ParentColorUpdater.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParentColorUpdater : MonoBehaviour {
+
+
+	private Renderer[] rend;
+	// Use this for initialization
+	void Start () {
+		rend = this.gameObject.GetComponentsInChildren<Renderer> ();
+	}
+	
+
+	void FixedUpdate () {
+	
+		ColorCondition cd = this.gameObject.GetComponentInParent<ColorCondition> ();
+
+		if (cd != null) {
+
+			foreach (Renderer r in rend) {
+				r.material.color= cd.color.color;
+			}
+		
+		}
+
+
+	}
+
+}
