e3c09a03f6b774690f1ebb3c1bf11be6565e25a6
Corvax-Fisher <martin.heller.mail@gmail.com>
2016-11-19 18:05:21 +0100

    Next level will be loaded, inverted horizontal axis

--- GameJam/Assets/Exit.cs
+++ GameJam/Assets/Exit.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Exit : MonoBehaviour {
 
   public Text exitText;
+  public string nextSceneName;
 
 	// Use this for initialization
 	void Start () {
@@ -11,8 +14,16 @@ public class Exit : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void OnTriggerEnter()
+	void OnTriggerEnter(Collider other)
   {
     exitText.text = "You sprang it!";
+    other.gameObject.SetActive(false);
+    if(!String.IsNullOrEmpty(nextSceneName))
+      Invoke("LoadNextLevel", 3);
+  }
+
+  void LoadNextLevel()
+  {
+    SceneManager.LoadScene(nextSceneName);
   }
 }
--- GameJam/Assets/InputController.cs
+++ GameJam/Assets/InputController.cs
@@ -22,7 +22,7 @@ public class InputController : MonoBehaviour
 
   void FixedUpdate()
   {
-    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveHorizontal = -Input.GetAxis("Horizontal");
     float moveVertical = Input.GetAxis("Vertical");
 
     //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
